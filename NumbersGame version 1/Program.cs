2d081c785dbaf7c2dc1e21dae747d2ecfc13d860
charl <charl@LAPTOP-QU0DA4M8>
2022-09-21 09:46:02 +0200

    Add project files.

---
+++ NumbersGame version 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumbersGame_version_1
+{
+    class Program
+    {
+        public static int RandomNumberGenerator(int randomNumber)
+        {
+            Random random = new Random();
+            randomNumber = random.Next(1, 20);
+
+            return randomNumber;
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Gissa vilket? Du får fem försök ");
+
+            RandomNumberGenerator();
+
+            
+
+            
+
+
+        }
+
+    }
+}
